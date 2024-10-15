70ff46ebcd4d298277c6ce58d27ddeb6c2a27a98
taeyeon0319 <xodus1349@likelion.org>
2024-10-15 16:32:22 +0900

    feat(엔티티): User엔티티 생성

---
+++ src/main/java/com/Familyship/checkkuleogi/domains/user/domain/SiteUser.java
@@ -0,0 +1,43 @@
+package com.Familyship.checkkuleogi.domains.user.domain;
+
+import com.Familyship.checkkuleogi.domains.user.domain.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name="user")
+public class SiteUser extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_idx")
+    private Long idx;
+
+    @Column(name = "user_name", nullable = false)
+    private String name;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private String id;
+
+    @Column(name = "user_password", nullable = false)
+    private String password;
+
+    @Column(name = "user_birth")
+    private LocalDate birth;
+
+    @Column(name = "user_gender")
+    private String gender;
+
+    @Column(name = "user_email", unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role")
+    private Role role;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Child> children;
+}
---
+++ src/main/java/com/Familyship/checkkuleogi/domains/user/domain/enums/Role.java
@@ -0,0 +1,6 @@
+package com.Familyship.checkkuleogi.domains.user.domain.enums;
+
+public enum Role {
+    USER,    // 회원
+    ADMIN    // 관리자
+}
