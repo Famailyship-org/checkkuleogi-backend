c15dab66ce62a6c697c81d44f3d5ef0b61447787
YaeJinKwon <93317276+YaeJinKwon@users.noreply.github.com>
2024-10-15 16:03:31 +0900

    feat(프로젝트): 수정

---
+++ src/main/java/com/Familyship/checkkuleogi/CheckkuleogiApplication.java
@@ -0,0 +1,13 @@
+package com.Familyship.checkkuleogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckkuleogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckkuleogiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Familyship/checkkuleogi/CheckkuleogiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Familyship.checkkuleogi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckkuleogiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
