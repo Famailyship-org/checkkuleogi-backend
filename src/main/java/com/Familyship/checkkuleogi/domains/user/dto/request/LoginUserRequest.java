59e3905465d3437f1532a183e0d9ebf0beca9515
daase <abstar@gachon.ac.kr>
2024-10-21 02:07:30 +0900

    feat(로그인):로그인에 사용될 Dto 추가.

---
+++ src/main/java/com/Familyship/checkkuleogi/domains/user/dto/request/LoginUserRequest.java
@@ -0,0 +1,9 @@
+package com.Familyship.checkkuleogi.domains.user.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class LoginUserRequest {
+    private String id;
+    private String password;
+}
